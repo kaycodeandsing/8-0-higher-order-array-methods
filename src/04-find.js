74cad345a2ebbbcad7e37f257f6eb257a4a14d40
Karimah Reavis <Karimah586@gmail.com>
2022-11-20 13:52:19 -0500

    finished lab

--- src/04-find.js
+++ src/04-find.js
@@ -13,21 +13,38 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findPinkElephantsByTimestreet(songs) {}
+function findPinkElephantsByTimestreet(songs) {
+ 
+ const foundsong = songs.find((song) => 
+ song.title === "Pink Elephants"
+);
+return foundsong
+}
 
 /**
  * Returns the first song in the list that is under three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+ const foundSong2 = songs.find((song) => 
+    song.runtimeInSeconds < 180
+   );
+   return foundSong2
+}
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstTitleTrack(songs) {}
+function findFirstTitleTrack(songs) {
+ const foundSong3 = songs.find((song) => 
+    song.title === song.album
+   
+    );
+    return foundSong3
+}
 
 module.exports = {
   findPinkElephantsByTimestreet,
--- src/06-sort.js
+++ src/06-sort.js
@@ -7,13 +7,18 @@
 */
 const exampleSongData = require("../data/songs");
 // Do not change the line above.
-
+// Use sort to sort an array. Pass a callback that compares two elements and returns either less than 0,0, or greater than 0 to determine the order. sort returns the original array after it's been mutated.
 /**
  * Reorders the array so that the song objects are organized by their runtime. The shortest song should come first.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  songs.sort((a, b) => {
+    return a.runtimeInSeconds - b.runtimeInSeconds
+  })
+  return songs
+}
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
@@ -23,8 +28,20 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
-
+function sortByArtistNameDescending(songs) {
+  songs.sort((a, b) => {
+    const songA = a.artist.toLowerCase()
+    const songB = b.artist.toLowerCase()
+    if (songA > songB) {
+      return -1;
+    }
+    if (songA < songB) {
+      return 1;
+    }
+    return  0
+})
+  return songs
+}
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
  *
@@ -33,7 +50,20 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+  songs.sort((a,b) => {
+    const songA = a.title.toLowerCase()
+    const songB = b.title.toLowerCase()
+    if (songA < songB) {
+      return -1;
+    }
+    if (songA > songB) {
+      return 1;
+    }
+    return  0
+})
+  return songs
+}
 
 module.exports = {
   sortByRuntimeAscending,
